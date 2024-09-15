f43fabcd170f59822b9029b629c292e85e8e2a48
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-09-15 20:24:09 +0200

    learning

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,27 @@
+import {db} from '@/db'
+import {notFound} from "next/navigation";
+
+interface SnippetShowPageProps {
+    params: {
+        id: string,
+    }
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+    const snippet = await db.snippet.findFirst({
+        where: {
+            id: parseInt(props.params.id)
+        }
+    });
+
+    if (!snippet) {
+        notFound();
+    }
+    return (
+        <div>
+            {snippet.code}
+        </div>
+    );
+
+
+}
